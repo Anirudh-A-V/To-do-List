b35c50529940d8dba51bd0d3479d24a4b00d769e
Anirudh A V <anirudh.av02@gmail.com>
2023-02-22 18:37:06 +0530

    feat: Created context provider

---
+++ contexts/ContextProvider.js
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+
+export const ContextProvider = ({ children }) => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = (title, description) => {
+        setTasks([...tasks, { title: title, description: description }]);
+        setTitle('');
+        setDescription('');
+        
+    }
+
+    return (
+        <StateContext.Provider
+            value={{
+                title,
+                setTitle,
+                description,
+                setDescription,
+                tasks,
+                setTasks,
+                addTask
+            }}
+        >
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext);
