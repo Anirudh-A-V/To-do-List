9e55b4243fdd56d60373517ac8ae2b4615d6391d
Anirudh A V <anirudh.av02@gmail.com>
2023-02-22 13:28:10 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [counter,setCounter]=useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{
+        fontSize: 35,
+        fontWeight: 'bold',
+        color: 'red',
+      }}>Hi</Text> 
+       
+       <Text style={{
+        fontSize: 95,
+        fontWeight: 'bold',
+        color: 'blue',
+      }}>{counter}</Text> 
+<TouchableOpacity onLongPress={()=>{setCounter(counter+1)}} onPress={()=>{setCounter(counter+1)}}  style={{
+        width: "100%",
+        backgroundColor: 'red',
+        padding: 10,
+     
+        alignItems:"center"
+      }} >
+  <Text style={{ color:"white",
+        fontSize: 20,}}>
+  Click Me
+  </Text>
+</TouchableOpacity>
+     
       <StatusBar style="auto" />
     </View>
   );
